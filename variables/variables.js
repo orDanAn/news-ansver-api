7e392b27c23ed9b021dee1882f23fa1068aab07f
Danil <ordanan38@gmail.com>
2020-03-31 13:29:26 +0800

    cors http

--- variables/variables.js
+++ variables/variables.js
@@ -7,7 +7,7 @@ const messegErrorServer = 'На сервере произошла ошибка';
 const messegNotContainID = 'карточки с таким id нет в базе';
 const messagLogout = 'До скорой встречи';
 const duplicateUser = 'такой пользователь уже есть';
-const whitelist = ['http://localhost:8080', 'https://www.news-ansver.site', 'https://ordanan.github.io'];
+const whitelist = ['http://localhost:8080', 'https://www.news-ansver.site', 'http://www.news-ansver.site', 'https://news-ansver.site', 'http://news-ansver.site', 'https://ordanan.github.io'];
 
 
 module.exports = {
